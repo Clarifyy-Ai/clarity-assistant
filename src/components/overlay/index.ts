2922c3eced5ca08fe1c30bcd0d8d390f7cc2d060
Clarifyy-Ai <clarifyai999@gmail.com>
2026-03-19 15:11:29 +0530

    Create index.ts

---
+++ src/components/overlay/index.ts
@@ -0,0 +1,22 @@
+// Barrel exports for all overlay components
+export { OverlayHintPanel } from './OverlayHintPanel';
+export { OverlayKeyboardHandler } from './OverlayKeyboardHandler';
+export { OverlayNetworkBadge } from './OverlayNetworkBadge';
+export { OverlayPositionManager } from './OverlayPositionManager';
+export { OverlayQuestionBar } from './OverlayQuestionBar';
+export { OverlayWindow } from './OverlayWindow';
+export { StealthMouseGuard } from './StealthMouseGuard';
+export { ScreenCaptureBlocker } from './ScreenCaptureBlocker';
+export { WindowVisibilityManager } from './WindowVisibilityManager';
+export { OverlaySettings } from './OverlaySettings';
+
+// Type exports
+export type { OverlayHintPanelProps } from './OverlayHintPanel';
+export type { OverlayKeyboardHandlerProps } from './OverlayKeyboardHandler';
+export type { OverlayNetworkBadgeProps } from './OverlayNetworkBadge';
+export type { OverlayPositionManagerProps } from './OverlayPositionManager';
+export type { OverlayQuestionBarProps } from './OverlayQuestionBar';
+export type { StealthMouseGuardProps } from './StealthMouseGuard';
+export type { ScreenCaptureBlockerProps, CaptureState } from './ScreenCaptureBlocker';
+export type { WindowVisibilityManagerProps } from './WindowVisibilityManager';
+export type { OverlaySettingsProps } from './OverlaySettings';
