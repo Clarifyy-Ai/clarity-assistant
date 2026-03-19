7d48c387fa43db4d7b2950b58cfcf9b4aaf94888
Clarifyy-Ai <clarifyai999@gmail.com>
2026-03-19 15:14:35 +0530

    Create index.ts

---
+++ src/components/onboarding/index.ts
@@ -0,0 +1,6 @@
+// Barrel exports for onboarding components
+export { OnboardingProgress } from './OnboardingProgress';
+export { OnboardingWizard } from './OnboardingWizard';
+
+// Type exports
+export type { OnboardingWizardProps } from './OnboardingWizard';
