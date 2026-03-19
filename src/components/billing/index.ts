0c7a72609de8aa479ced971f675f38c4e9a8f63e
Clarifyy-Ai <clarifyai999@gmail.com>
2026-03-19 15:19:12 +0530

    Create index.ts

---
+++ src/components/billing/index.ts
@@ -0,0 +1,10 @@
+// Barrel exports for billing components
+export { UpgradeModal } from './UpgradeModal';
+export { CreditBalance } from './CreditBalance';
+export { PricingCard } from './PricingCard';
+export { BillingHistory } from './BillingHistory';
+
+// Type exports
+export type { PricingCardProps } from './PricingCard';
+export type { CreditBalanceProps } from './CreditBalance';
+export type { BillingHistoryProps } from './BillingHistory';
